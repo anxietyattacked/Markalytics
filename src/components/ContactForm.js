ab9b2991977ea5846794dbf7def6cca196d273dd
Michael Bergerson <michealbergerson@gmail.com>
2021-02-06 11:03:48 -0800

    contact connected

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 const ContactForm = () => {
     return (
-        <form className="contact-form">
+        <form form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="botfield" className="contact-form">
+            <input type="hidden" name="form-name" value="contact"></input>
             <div className="label-div">
-            <label>Name</label>
+            <label htmlFor="name">Name</label>
             </div>
-            <input type="text" name="name" className="Name" required/>
+            <input  type="text" name="name" className="Name" required/>
             <div className="label-div">
-            <label>Email</label>
+            <label htmlFor="email">Email</label>
             </div>
             <input type="email" name="email" className="Email" required/>
+            <div hidden> <input name="bot-field" type="text"/></div>
             <div className="label-div">
-            <label>Message</label>
+            <label htmlFor="message">Message</label>
             </div>
-            <textarea className="text-area" required/>
+            <textarea  name="message" className="text-area" required/>
             <button className="submit-btn">Submit</button>
         </form>
     )
