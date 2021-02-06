9bf9f725926304a6e9d33e86ddc369e9e179e216
Michael Bergerson <michealbergerson@gmail.com>
2021-02-06 12:10:27 -0800

    contact change v4

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactForm = () => {
     return (
-        <form name="contact" method="POST" netlify className="contact-form">
+        <form name="contact" method="POST" netlify netlify-honeypot="bot-field"  className="contact-form">
             <input type="hidden" name="form-name" value="contact"></input>
             <div className="label-div">
             <label htmlFor="name">Name</label>
